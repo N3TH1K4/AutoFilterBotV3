5d1d7836ad097630caf8dd82b148000d67c5049c
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-06-24 07:45:59 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -50,7 +50,7 @@ async def filter(client, message):
         #return
     if 2 < len(message.command) < 100:    
         btn = []
-        search = message.text
+        search = message.text.split(None, 1)[1]
         mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by {message.chat.title}**"
         files = await get_filter_results(query=search)
         if files:
@@ -112,7 +112,7 @@ async def group(client, message):
         #return
     if 2 < len(message.command) < 50:    
         btn = []
-        search = message.text
+        search = message.text.split(None, 1)[1]
         mo_tech_yt = f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by {message.chat.title}**"
         nyva=BOT.get("username")
         if not nyva:
