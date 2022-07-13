33500c1e8e654da014f2c300a924e98aa33d819d
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-07-13 20:00:53 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,33 +5,33 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'LuciferMoringstar_Robot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+SESSION = 'LuciferMoringstar_Robot'
+API_ID = 18983092
+API_HASH = "a6005a70e88369b4fb08b8350ebbdd35"
+BOT_TOKEN = "1977805563:AAFgr7EsLJHqBTo8jOSxbU6bH8AMM8nTzTg"
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
 
-BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
-ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
-DB_URL = os.environ.get("DATABASE_1", "")
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
+BROADCAST_CHANNEL = -1001558732171
+ADMIN_ID = 1930645496
+DB_URL = "https://t.me/+g9A9yU6uaJg2YTdl"
+BROADCAST_AS_COPY = True
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+ADMINS = 1930645496
+CHANNELS = -1001553755993
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('FORCES_SUB')
+auth_channel = ""
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 TUTORIAL = "https://youtu.be/5hnYOKBzyi8"
 # MongoDB information
-DATABASE_URI = environ['DATABASE_2']
-DATABASE_NAME = environ['BOT_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = "Tets"
+COLLECTION_NAME = 'Telegram_files'
 
 # Messages
 default_start_msg = """
@@ -41,8 +41,8 @@ Here you can search files in Inline mode as well as PM, Use the below buttons to
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
-FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
-OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+FILE_CAPTION = ""
+OMDB_API_KEY = ""
 if FILE_CAPTION.strip() == "":
     CUSTOM_FILE_CAPTION=None
 else:
